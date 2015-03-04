e18c552b8768c33d327752b82e86f162dc81f4eb
vida-z <vida.zolghadr@gmail.com>
2015-03-04 18:15:00 +0330

    code version yashil2
    wireless communication changed(not tested yet)

--- base-FPGA/src/main.c
+++ base-FPGA/src/main.c
@@ -33,12 +33,18 @@
 #include "transmitter.h" //warning mide az sendnewdata va senddata
 #include <stdlib.h>
 
+void NRF_init (void) ;
+void data_transmission (void);
+
 unsigned char Buf_Rx_L[_Buffer_Size] ;
 char Buf_Tx_L[_Buffer_Size];
 char Address[_Address_Width] = { 0x11, 0x22, 0x33, 0x44, 0x55};
 int motor_num=0,test=0;
-uint32_t kck_time;
+uint32_t kck_time,time_test=0;
 int free_wheel=0;
+int wireless_reset=0;
+int Robot_Select,Robot_Select_Last;
+int Test_Data[8];
 int adc =0;
 int flg=0;
 
@@ -65,40 +71,14 @@ int main (void)
 	// Globally enable interrupts
 	sei();
 
-	Address[0]=Address[0] + RobotID ;
+	//Address[0]=Address[0] + RobotID ;
 
-	///////////////////////////////////////////////////////////////////////////////////////////////Begin NRF Initialize
-	NRF24L01_L_CE_LOW;       //disable transceiver modes
-
-	SPI_Init();
-
-	_delay_us(10);
-	_delay_ms(100);      //power on reset delay needs 100ms
-	NRF24L01_L_Clear_Interrupts();
-	NRF24L01_L_Flush_TX();
-	NRF24L01_L_Flush_RX();
-	NRF24L01_L_CE_LOW;
-	if (RobotID < 3)
-	NRF24L01_L_Init_milad(_TX_MODE, _CH_0, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
-	else if(RobotID > 2 && RobotID < 6)
-	NRF24L01_L_Init_milad(_TX_MODE, _CH_1, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
-   else if(RobotID > 5 && RobotID < 9)
-	NRF24L01_L_Init_milad(_TX_MODE, _CH_2, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
-	else
-	NRF24L01_L_Init_milad(_TX_MODE, _CH_3, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	//// NRF Initialize
+	NRF_init () ;
 	
-	NRF24L01_L_WriteReg(W_REGISTER | DYNPD,0x01);
-	NRF24L01_L_WriteReg(W_REGISTER | FEATURE,0x06);
-
-	NRF24L01_L_CE_HIGH;
-	_delay_us(130);
-	/////////////////////////////////////////////////////////////////////////////////////////////END   NRF Initialize
 	while(1)
 	  {  
-		  //if(((KCK_DCh_Limit_PORT.IN & KCK_DCh_Limit_PIN_bm)>>KCK_DCh_Limit_PIN_bp))
-		  //{
-			  //LED_Green_PORT.OUTTGL = LED_Green_PIN_bm;
-		  //}
+		    asm("wdr");
 		   // BUZZER
 		    adc = adc_get_unsigned_result(&ADCA,ADC_CH0);
 		   //adc = 1200;
@@ -113,12 +93,10 @@ int main (void)
 			
 			//SHOOT
 			PORTC_OUTCLR=KCK_SH_PIN_bm;
-			//tc_enable_cc_channels(&TCC0,TC_CCDEN);
 			if((KCK_Ch_Limit_PORT.IN & KCK_Ch_Limit_PIN_bm)>>KCK_Ch_Limit_PIN_bp)
 			{
 				full_charge=1;
 				tc_disable_cc_channels(&TCC0,TC_CCDEN);
-				//LED_White_PORT.OUTTGL = LED_White_PIN_bm;
 			}
 			else
 			{
@@ -137,7 +115,10 @@ int main (void)
 			if (KCK_DSH_SW)//bazi vaghta begir nagir dare
 			{
 				flg = 1;
-				//LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
+			}
+			if (free_wheel >= 500 )
+			{
+				NRF_init();
 			}
 			
 		   // //motor test
@@ -242,37 +223,7 @@ int main (void)
 		  //Robot_D[RobotID].M2a  = 0X03;//high
 		  //Robot_D[RobotID].M3b  = 0xB8;//3000//32;//ghalat30258
 		  //Robot_D[RobotID].M3a  = 0X0B;//76;
-		  
-		  //LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
-		 // (Robot_D[RobotID].M0a == 1) && (Robot_D[RobotID].M0b == 2) && (Robot_D[RobotID].M1a==3) && (Robot_D[RobotID].M1b == 4) ||
-		//  if ( free_wheel>100)
-		  //{  
-			  //Robot_D[RobotID].M0a = 1;
-			  //Robot_D[RobotID].M0b = 2;
-			  //Robot_D[RobotID].M1a = 3;
-			  //Robot_D[RobotID].M1b = 4;
-		 // }
-		  free_wheel++;
-		  
-		  
-		    asm("wdr");
-		    if (ctrlflg)
-		    {
-			    ctrlflg = 0;
-				
-				Buf_Tx_L[0] = Robot_D[RobotID].M0a;
-				Buf_Tx_L[1] = Robot_D[RobotID].M0b;
-				Buf_Tx_L[2] = Robot_D[RobotID].M1a;
-				Buf_Tx_L[3] = Robot_D[RobotID].M1b;
-				Buf_Tx_L[4] = Robot_D[RobotID].M2a;
-				Buf_Tx_L[5] = Robot_D[RobotID].M2b;
-				Buf_Tx_L[6] = Robot_D[RobotID].M3a;
-				Buf_Tx_L[7] = Robot_D[RobotID].M3b;
-			    NRF24L01_L_Write_TX_Buf(Buf_Tx_L,_Buffer_Size);
-			    NRF24L01_L_RF_TX();
-		    }
-		    _delay_us(1);
-			
+		    
 			////SEND TEST DATA TO FT232
 			//char str1[20];
 		    //uint8_t count1 = sprintf(str1,"%d\r",adc);
@@ -289,48 +240,85 @@ int main (void)
 
 ISR(PORTD_INT0_vect)////////////////////////////////////////PTX   IRQ Interrupt Pin
 {  
-	uint8_t status_L = NRF24L01_L_WriteReg(W_REGISTER | STATUSe, _TX_DS|_MAX_RT|_RX_DR);
-	  
-	if((status_L & _RX_DR) == _RX_DR)
-	{
-		LED_White_PORT.OUTTGL = LED_White_PIN_bm;
-		//1) read payload through SPI,
-		NRF24L01_L_Read_RX_Buf(Buf_Rx_L, _Buffer_Size);
-		free_wheel=0 ;
-		if(Buf_Rx_L[0] == RobotID)
-		{
-			Robot_D[RobotID].RID  = Buf_Rx_L[0];
-			Robot_D[RobotID].M0a  = Buf_Rx_L[1];
-			Robot_D[RobotID].M0b  = Buf_Rx_L[2];
-			Robot_D[RobotID].M1a  = Buf_Rx_L[3];
-			Robot_D[RobotID].M1b  = Buf_Rx_L[4];
-			Robot_D[RobotID].M2a  = Buf_Rx_L[5];
-			Robot_D[RobotID].M2b  = Buf_Rx_L[6];
-			Robot_D[RobotID].M3a  = Buf_Rx_L[7];
-			Robot_D[RobotID].M3b  = Buf_Rx_L[8];
-			Robot_D[RobotID].KCK  = Buf_Rx_L[9];
-			Robot_D[RobotID].CHP  = Buf_Rx_L[10];
-			Robot_D[RobotID].ASK  = Buf_Rx_L[11];
-			Robot_D[RobotID].P    = Buf_Rx_L[12];
-			Robot_D[RobotID].I    = Buf_Rx_L[13];
-			Robot_D[RobotID].D    = Buf_Rx_L[14];
-		}
+	 uint8_t status_L = NRF24L01_L_WriteReg(W_REGISTER | STATUSe, _TX_DS|_MAX_RT|_RX_DR);
+	 if((status_L & _RX_DR) == _RX_DR)
+	 {
+		 LED_White_PORT.OUTTGL = LED_White_PIN_bm;
+		 wireless_reset=0;
+		 //1) read payload through SPI,
+		 NRF24L01_L_Read_RX_Buf(Buf_Rx_L, _Buffer_Size);
+		 free_wheel=0 ;
+		 if(Buf_Rx_L[0] == 'L')//RobotID)
+		 {
+			 LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
+			 Robot_D[RobotID].RID  = Buf_Rx_L[0];
+			 Robot_D[RobotID].M0a  = Buf_Rx_L[1+ RobotID%3 * 10];
+			 Robot_D[RobotID].M0b  = Buf_Rx_L[2+ RobotID%3 * 10];
+			 Robot_D[RobotID].M1a  = Buf_Rx_L[3+ RobotID%3 * 10];
+			 Robot_D[RobotID].M1b  = Buf_Rx_L[4+ RobotID%3 * 10];
+			 Robot_D[RobotID].M2a  = Buf_Rx_L[5+ RobotID%3 * 10];
+			 Robot_D[RobotID].M2b  = Buf_Rx_L[6+ RobotID%3 * 10];
+			 Robot_D[RobotID].M3a  = Buf_Rx_L[7+ RobotID%3 * 10];
+			 Robot_D[RobotID].M3b  = Buf_Rx_L[8+ RobotID%3 * 10];
+			 Robot_D[RobotID].KCK  = Buf_Rx_L[9+ RobotID%3 * 10];
+			 Robot_D[RobotID].CHP  = Buf_Rx_L[10+RobotID%3 * 10];
+			 Robot_D[RobotID].ASK  = Buf_Rx_L[31];//0b00000000
+			 
+			 //if (rpm_ready[0].data_permit)
+			 //{
+			 //rpm_ready[0].Ma = Robot_D[RobotID].M0a;
+			 //rpm_ready[0].Mb = Robot_D[RobotID].M0b;
+			 //}
+			 //if (rpm_ready[1].data_permit)
+			 //{
+			 //rpm_ready[1].Ma = Robot_D[RobotID].M1a;
+			 //rpm_ready[1].Mb = Robot_D[RobotID].M1b;
+			 //}
+			 //if (rpm_ready[2].data_permit)
+			 //{
+			 //rpm_ready[2].Ma = Robot_D[RobotID].M2a;
+			 //rpm_ready[2].Mb = Robot_D[RobotID].M2b;
+			 //}
+			 //if (rpm_ready[3].data_permit)
+			 //{
+			 //rpm_ready[3].Ma = Robot_D[RobotID].M3a;
+			 //rpm_ready[3].Mb = Robot_D[RobotID].M3b;
+			 //}
+			 
+			 if (Robot_D[RobotID].ASK != Robot_Select)
+			 {
+				 Robot_Select = Robot_D[RobotID].ASK;
+				 if (Robot_Select == RobotID)
+				 {
+					 NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x01);
+				 }
+				 else
+				 {
+					 NRF24L01_L_WriteReg(W_REGISTER | EN_AA, 0x00);
+				 }
+			 }
+			 
+			 if (Robot_D[RobotID].ASK == RobotID)
+			 {
+				 data_transmission();
+			 }
 
-		//2) clear RX_DR IRQ,
-		//NRF24L01_R_WriteReg(W_REGISTER | STATUSe, _RX_DR );
-		//3) read FIFO_STATUS to check if there are more payloads available in RX FIFO,
-		//4) if there are more data in RX FIFO, repeat from step 1).
-	}
-	if((status_L&_TX_DS) == _TX_DS)
-	{   LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
-		//NRF24L01_R_WriteReg(W_REGISTER | STATUSe, _TX_DS);
-	}
-	if ((status_L&_MAX_RT) == _MAX_RT)
-	{
-		LED_Green_PORT.OUTTGL = LED_Green_PIN_bm;
-		NRF24L01_L_Flush_TX();
-		//NRF24L01_R_WriteReg(W_REGISTER | STATUSe, _MAX_RT);
-	}
+		 }
+		 //2) clear RX_DR IRQ,
+		 //NRF24L01_R_WriteReg(W_REGISTER | STATUSe, _RX_DR );
+		 //3) read FIFO_STATUS to check if there are more payloads available in RX FIFO,
+		 //4) if there are more data in RX FIFO, repeat from step 1).
+	 }
+	 if((status_L&_TX_DS) == _TX_DS)
+	 {
+		 LED_Green_PORT.OUTTGL = LED_Green_PIN_bm;
+		 wireless_reset=0;
+	 }
+	 
+	 if ((status_L&_MAX_RT) == _MAX_RT)
+	 {
+		 NRF24L01_L_Flush_TX();
+	 }
 }
 
 char timectrl,time2sec;
@@ -342,6 +330,7 @@ ISR(TCE1_OVF_vect)//1ms
 		ctrlflg=1;
 		timectrl=0;
 	}
+	wireless_reset++;
 	//time2sec++;
 	//if (time2sec>=10)
 	//{
@@ -355,10 +344,7 @@ ISR(TCE1_OVF_vect)//1ms
 			kck_time++;
 			tc_disable_cc_channels(&TCC0,TC_CCDEN);
 			if(((KCK_DCh_Limit_PORT.IN & KCK_DCh_Limit_PIN_bm)>>KCK_DCh_Limit_PIN_bp))
-			//if(KCK_DCh_Limit) // chera moshkel dare?!
 				tc_disable_cc_channels(&TCC0,TC_CCCEN);
-				
-				
 			else
 			{
 				if(KCK_DSH_SW)
@@ -398,6 +384,13 @@ ISR(TCD0_OVF_vect)
 
 ISR(TCD0_CCA_vect)
 {   
+	if ( free_wheel>100)
+	{
+		Robot_D[RobotID].M0a = 1;
+		Robot_D[RobotID].M0b = 2;
+		Robot_D[RobotID].M1a = 3;
+		Robot_D[RobotID].M1b = 4;
+	}
 	switch (motor_num)
 	{
 		case 0 :
@@ -528,82 +521,72 @@ ISR(TCD0_CCA_vect)
 	  //if (motor_num>=16)
 	  //motor_num=0;
 	}
-//ISR(TCD0_CCA_vect)
-//{    
-	//if (timeFPGA==5)
-	  //{   
-		  ////LED_White_PORT.OUTTGL = LED_White_PIN_bm;
-	    //switch (motor_num)
-	    //{
-		    //case 0 :
-				//MOTORNUM_PORT.OUTCLR= (MOTORNUM0_bm | MOTORNUM1_bm);
-				//if((CLK_par_PORT.IN && CLK_par_bm)==0)
-				//{   LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
-					//FPGA_DATA_PORT.OUT = Robot_D[RobotID].M0a;
-					//PARITY_PORT.OUTSET =(parity_calc(Robot_D[RobotID].M0a)<<PARITY_bp);
-					//motor_num=0;
-				//}
-				//else if((CLK_par_PORT.IN && CLK_par_bm)==1)
-				//{
-					//FPGA_DATA_PORT.OUT = Robot_D[RobotID].M0b;
-					//PARITY_PORT.OUTSET = (parity_calc(Robot_D[RobotID].M0b)<<PARITY_bp); 
-					//LED_White_PORT.OUTTGL = LED_White_PIN_bm;
-					//motor_num=1;
-				//} 
-		    //break;
-		    //case 1 :
-			   //MOTORNUM_PORT.OUT= MOTORNUM0_bm;
-			   //if((CLK_par_PORT.IN && CLK_par_bm)==0)
-			   //{
-				   //FPGA_DATA_PORT.OUT = Robot_D[RobotID].M1a;
-				   //PARITY_PORT.OUTSET = (parity_calc(Robot_D[RobotID].M1a)<<PARITY_bp);
-				   //motor_num=1;
-			   //}
-			   //else if((CLK_par_PORT.IN && CLK_par_bm)==1)
-			   //{
-				   //FPGA_DATA_PORT.OUT = Robot_D[RobotID].M1b;
-				   //PARITY_PORT.OUTSET = (parity_calc(Robot_D[RobotID].M1b)<<PARITY_bp);
-				   //motor_num=2;
-			   //}
-		    //break;
-		    //case 2 :
-			   //MOTORNUM_PORT.OUT= MOTORNUM1_bm;
-			   //if((CLK_par_PORT.IN && CLK_par_bm)==0)
-			   //{
-				   //FPGA_DATA_PORT.OUT = Robot_D[RobotID].M2a;
-				   //PARITY_PORT.OUTSET = (parity_calc(Robot_D[RobotID].M2a)<<PARITY_bp);
-				   //motor_num=2;
-			   //}
-			   //else if((CLK_par_PORT.IN && CLK_par_bm)==1)
-			   //{
-				   //FPGA_DATA_PORT.OUT = Robot_D[RobotID].M2b;
-				   //PARITY_PORT.OUTSET = (parity_calc(Robot_D[RobotID].M2b)<<PARITY_bp);
-				   //motor_num=3;
-			   //}
-		    //break;
-		    //case 3 :
-				//MOTORNUM_PORT.OUT= (MOTORNUM0_bm | MOTORNUM1_bm);
-				//if((CLK_par_PORT.IN && CLK_par_bm)==0)
-				//{
-					//FPGA_DATA_PORT.OUT = Robot_D[RobotID].M3a;
-					//PARITY_PORT.OUTSET = (parity_calc(Robot_D[RobotID].M3a)<<PARITY_bp);
-					//motor_num=3;
-				//}
-				//else if((CLK_par_PORT.IN && CLK_par_bm)==1)
-				//{
-					//FPGA_DATA_PORT.OUT = Robot_D[RobotID].M3b;
-					//PARITY_PORT.OUTSET = (parity_calc(Robot_D[RobotID].M3b)<<PARITY_bp);
-					////LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
-					//motor_num=0;
-				//}
-		    //break;
-	    //}	
-	//}
-//}
+
 int parity_calc(signed int data)
 {
 	int parity=0;
 	parity = (data & PIN0_bm) ^ ((data & PIN1_bm)>>PIN1_bp) ^ ((data & PIN2_bm)>>PIN2_bp) ^ ((data & PIN3_bm)>>PIN3_bp)
 	 ^ ((data & PIN4_bm)>>PIN4_bp) ^ ((data & PIN5_bm)>>PIN5_bp) ^ ((data & PIN6_bm)>>PIN6_bp) ^ ((data & PIN7_bm)>>PIN7_bp);
     return parity;
+}
+
+void NRF_init (void)
+{
+	NRF24L01_L_CE_LOW;       //disable transceiver modes
+
+	SPI_Init();
+
+	_delay_us(10);
+	_delay_ms(11);      //power on reset delay needs 10.3ms//amin changed 100ms to 11ms
+	NRF24L01_L_Clear_Interrupts();
+	NRF24L01_L_Flush_TX();
+	NRF24L01_L_Flush_RX();
+	NRF24L01_L_CE_LOW;
+	// 	    if (RobotID < 3)
+	// 	    NRF24L01_L_Init_milad(_TX_MODE, _CH_0, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	// 	    else if(RobotID > 2 && RobotID < 6)
+	// 	    NRF24L01_L_Init_milad(_TX_MODE, _CH_1, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	// 	    else if (RobotID > 5 && RobotID < 9)
+	// 	    NRF24L01_L_Init_milad(_TX_MODE, _CH_2, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	// 	    else
+	// 	    NRF24L01_L_Init_milad(_TX_MODE, _CH_3, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	if (RobotID < 3)
+	NRF24L01_L_Init_milad(_RX_MODE, _CH_1, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	else if(RobotID > 2 && RobotID < 6)
+	NRF24L01_L_Init_milad(_RX_MODE, _CH_0, _2Mbps, Address, _Address_Width, _Buffer_Size, RF_PWR_MAX);
+	NRF24L01_L_WriteReg(W_REGISTER | DYNPD,0x01);
+	NRF24L01_L_WriteReg(W_REGISTER | FEATURE,0x06);
+
+	NRF24L01_L_CE_HIGH;
+	_delay_us(130);
+}
+
+void data_transmission (void)
+{
+	//transmitting data to wireless board/////////////////////////////////////////////////
+	Test_Data[0] = adc/12;
+	Test_Data[1] = time_test;
+	
+	Buf_Tx_L[0]  = Robot_D[RobotID].M0a;//(Test_Data[0]>> 8) & 0xFF;	//drive test data
+	Buf_Tx_L[1]  = Robot_D[RobotID].M0b;//Test_Data[0] & 0xFF;			//drive test data
+	Buf_Tx_L[2]  = Robot_D[RobotID].M1a;//(Test_Data[1]>> 8) & 0xFF;	//drive test data
+	Buf_Tx_L[3]  = Robot_D[RobotID].M1b;//Test_Data[1] & 0xFF;			//drive test data
+	Buf_Tx_L[4]  = Robot_D[RobotID].M2a;//(Test_Data[2]>> 8) & 0xFF;	//drive test data
+	Buf_Tx_L[5]  = Robot_D[RobotID].M2b;//Test_Data[2] & 0xFF;			//drive test data
+	Buf_Tx_L[6]  = Robot_D[RobotID].M3a;//(Test_Data[3]>> 8) & 0xFF;	//drive test data
+	Buf_Tx_L[7]  = Robot_D[RobotID].M3b;//Test_Data[3] & 0xFF;			//drive test data
+	//Buf_Tx_L[8]  = (Test_Data[4]>> 8) & 0xFF;	// unused
+	//Buf_Tx_L[9]  = Test_Data[4] & 0xFF;			// unused
+	//Buf_Tx_L[10] = (Test_Data[5]>> 8) & 0xFF;// unused
+	//Buf_Tx_L[11] = Test_Data[5] & 0xFF;			// unused
+	//Buf_Tx_L[12] = (Test_Data[6]>> 8) & 0xFF;	// unused
+	//Buf_Tx_L[13] = Test_Data[6] & 0xFF;			// unused
+	//Buf_Tx_L[14] = (Test_Data[7]>> 8) & 0xFF;	// unused
+	//Buf_Tx_L[15] = Test_Data[7] & 0xFF;			// unused
+	Buf_Tx_L[16] = adc/12;						//battery voltage
+	
+
+	//LED_Red_PORT.OUTTGL = LED_Red_PIN_bm;
+	NRF24L01_L_Write_TX_Buf(Buf_Tx_L, _Buffer_Size);
+	//NRF24L01_L_RF_TX();
 }
